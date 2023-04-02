54a739d1c571e5d33fb80e35f9c819a5cae7e88f
James Isom <jamesisom310@gmail.com>
2023-04-02 00:57:05 -0400

    Updated home.html to home.php

--- nav.php
+++ nav.php
@@ -10,7 +10,7 @@
                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                          <li class="nav-item" target="_top">
-                             <a class="nav-link" aria-current="page" href="../home/home.html">Home</a>
+                             <a class="nav-link" aria-current="page" href="../home/home.php">Home</a>
                          </li>
                          <li class="nav-item" target="_top">
                              <a class="nav-link " href="../conversation/conversation.html">Messages</a>
